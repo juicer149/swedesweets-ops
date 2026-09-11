4b2268530696c0b5a808dbea27a43f996ae295f9
juicer149 <karlal_bin_jimmy@proton.me>
2026-09-11 14:41:54 +0200

    quantity stepper added to details(js)

--- static/js/catalog_detail.js
+++ static/js/catalog_detail.js
@@ -50,8 +50,114 @@ function initializeOfferStockHint() {
 }
 
 
+function initializeQuantityStepper() {
+  const stepper = document.querySelector(
+    "[data-quantity-stepper]"
+  );
+
+  if (!stepper) {
+    return;
+  }
+
+  const input = stepper.querySelector(
+    "[data-quantity-input]"
+  );
+
+  const decreaseButton = stepper.querySelector(
+    "[data-quantity-decrease]"
+  );
+
+  const increaseButton = stepper.querySelector(
+    "[data-quantity-increase]"
+  );
+
+  if (
+    !input
+    || !decreaseButton
+    || !increaseButton
+  ) {
+    return;
+  }
+
+  const minimum = Number.parseInt(
+    input.min || "1",
+    10
+  );
+
+  const readQuantity = () => {
+    const quantity = Number.parseInt(
+      input.value,
+      10
+    );
+
+    if (!Number.isInteger(quantity)) {
+      return null;
+    }
+
+    return quantity;
+  };
+
+  const renderState = () => {
+    const quantity = readQuantity();
+
+    decreaseButton.disabled = (
+      quantity === null
+      || quantity <= minimum
+    );
+  };
+
+  decreaseButton.addEventListener(
+    "click",
+    () => {
+      const quantity = readQuantity();
+
+      if (
+        quantity === null
+        || quantity <= minimum
+      ) {
+        return;
+      }
+
+      input.value = String(
+        quantity - 1
+      );
+
+      renderState();
+    }
+  );
+
+  increaseButton.addEventListener(
+    "click",
+    () => {
+      const quantity = readQuantity();
+
+      input.value = String(
+        quantity === null
+          ? minimum
+          : quantity + 1
+      );
+
+      renderState();
+    }
+  );
+
+  input.addEventListener(
+    "input",
+    renderState
+  );
+
+  input.addEventListener(
+    "change",
+    renderState
+  );
+
+  renderState();
+}
+
+
 function initializeCatalogDetail() {
   initializeOfferStockHint();
+  initializeQuantityStepper();
 }
 
 
